f0ff1630594f9231e90abffebe670ab2e62a4bce
hpcale02 <71714850+hpcale02@users.noreply.github.com>
2021-05-19 11:21:30 +0200

    v.fin_labirinto

--- c++/eserciziLibro/cap8/n25/Labirinto.cpp
+++ c++/eserciziLibro/cap8/n25/Labirinto.cpp
@@ -38,24 +38,69 @@ int main()
 void mazeTraverse(char lab[][12], int x, int y, int direction)
 {
     bool vittoria = false;
-    lab[x][y] = 'x';
-    displayLabirinto(lab);
+    int count = 0;
 
-    //vittoria
-    if ((x == 0 || x == 11) && (y >= 0 && y <= 11) && x != X_START && y != Y_START)
-        vittoria = true;
-    if ((y == 0 || y == 11) && (x >= 0 && x <= 11) && x != X_START && y != Y_START)
-        vittoria = true;
-    else
-        vittoria = false;
-
-    if (vittoria)
+    while (true)
     {
-        cout << "Sei riuscito a uscire, complimenti!" << endl;
-        return;
-    }
+        count++;
+        cout << count << "° PASSO" << endl;
+        lab[x][y] = 'x';
+        displayLabirinto(lab);
 
-    
+        switch (direction)
+        {
+        case DOWN:
+            if (lab[x][y + 1] == '.')
+            {
+                direction++;
+                break;
+            }
+            (lab[x + 1][y] != '#') ? x++ : direction++;
+            break;
+        case RIGHT:
+            if (lab[x - 1][y] == '.')
+            {
+                direction++;
+                break;
+            }
+            (lab[x][y + 1] != '#') ? y++ : direction++;
+            break;
+        case UP:
+            if (lab[x][y - 1] == '.')
+            {
+                direction++;
+                break;
+            }
+            (lab[x - 1][y] != '#') ? x-- : direction++;
+            break;
+        case LEFT:
+            if (lab[x + 1][y] == '.')
+            {
+                direction = DOWN;
+                break;
+            }
+            (lab[x][y - 1] != '#') ? y-- : direction = DOWN;
+            break;
+        default:
+            break;
+        }
+
+        //vittoria
+        if ((x == 0 || x == 11) && (y >= 0 && y <= 11) && x != X_START && y != Y_START)
+            vittoria = true;
+        if ((y == 0 || y == 11) && (x >= 0 && x <= 11) && x != X_START && y != Y_START)
+            vittoria = true;
+        else
+            vittoria = false;
+
+        if (vittoria)
+        {
+            lab[x][y] = 'x';
+            displayLabirinto(lab);
+            cout << "Sei riuscito a uscire, complimenti!" << endl;
+            return;
+        }
+    }
 }
 
 void displayLabirinto(const char labirinto[][12])
